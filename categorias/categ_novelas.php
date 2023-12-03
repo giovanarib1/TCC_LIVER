dbbe169172dd1c7f8f425b55bb17b2b6a534ad04
giovanarib1 <129679849+giovanarib1@users.noreply.github.com>
2023-12-03 12:12:41 -0300

    Add files via upload

--- categorias/categ_novelas.php
+++ categorias/categ_novelas.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="../imagens/logo2_liver.png" type="image/x-icon">
-        <title>LiVer</title>
+        <title>Novelas</title>
         <!-- Pesquisar CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
         <!-- Swiper CSS -->
@@ -13,7 +13,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css">
         <!-- CSS -->
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../categorias/categorias.css">
                                         
     </head>
     <body>
@@ -24,48 +24,65 @@
     <div class="container-fluid">
     <!--NAVBAR-->
     <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark w-100">
-    <div class="container">
-        <a class="navbar-brand">
-            <img src="../imagens/liver_logo.png" alt="Logo LiVer" src="index.php" width="100" height="40"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse me-auto y-2 my-lg-0" id="navbarScroll">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../index.php">Início</a>
-            </li>
-            <li><hr class="dropdown-divider"></li>
-            <li class="nav-item">
-            <a class="nav-link" href="../login/index.html">Login</a>
-            </li>
-            <li><hr class="dropdown-divider"></li>
-            <li class="nav-item">
-            <a class="nav-link" href="../citacoes/citacoes.php">Citações</a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
-            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                <li><a class="dropdown-item" href="./categ_livros.php">Livros</a></li>
-                <li><a class="dropdown-item" href="./categ_filmes.php">Filmes</a></li>
-                <li><a class="dropdown-item" href="./categ_series.php">Séries</a></li>
-                <li><a class="dropdown-item" href="./categ_novelas.php">Novelas</a></li>
-            </ul>
-            </li>
-        </ul>
+        <div class="container">
+            <a class="navbar-brand">
+                <img src="../imagens/liver_logo.png" alt="Logo LiVer" src="../index.php" width="100" height="40"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Início</a>
+                    </li>
+                    <li><hr class="dropdown-divider"></li>
+                    <?php
+                    // Inicie a sessão se ainda não estiver iniciada
+                    session_start();
+
+                    // Verifique se o ID_USUARIO está na sessão
+                    if(isset($_SESSION['ID_USUARIO']) && !empty($_SESSION['ID_USUARIO'])) {
+                        // O ID_USUARIO está configurado na sessão
+                        echo '<li class="nav-item">
+                                <a class="nav-link" href="../perfil/perfil_user.php">Meu perfil</a>
+                            </li>';
+                    } else {
+                        // O ID_USUARIO não está configurado na sessão
+                        echo '<li class="nav-item">
+                                <a class="nav-link" href="../login/index.html">Login</a>
+                            </li>';
+                    }
+                    ?>
+                    <li><hr class="dropdown-divider"></li>
+                    <li class="nav-item">
+                         <a class="nav-link" href="../citacoes/citacoes.php">Citações</a>
+                   </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                            <li><a class="dropdown-item" href="../categorias/categ_livros.php">Livros</a></li>
+                            <li><a class="dropdown-item" href="../categorias/categ_filmes.php">Filmes</a></li>
+                            <li><a class="dropdown-item" href="../categorias/categ_series.php">Séries</a></li>
+                            <li><a class="dropdown-item" href="../categorias/categ_novelas.php">Novelas</a></li>
+                        </ul>
+                    </li>
+                </ul>
+
+            
+                <form class="d-flex" action="../pesquisar.php" method="GET">
+                    <div class="search-box"> 
+                        <input class="search-txt" type="text" placeholder="Pesquisar" aria-label="Pesquisar" name="pesquisar">
+                        <a class="search-btn" href="#">
+                            <i class="fas fa-search"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
-        <form class="d-flex" action="../pesquisar.php" method="GET">
-        <div class="search-box"> 
-        <input class="search-txt" type="text" placeholder= "     Pesquisar" aria-label="Pesquisar" name="pesquisar">
-            <a class="search-btn" href="#">
-            <i class="fas fa-search"></i>
-            </a>
-        </div>
-        </form>
-    </div>
     </nav>
     <br><br><br><br>
-<!--NAVBAR TERMINA-->
+    <!--NAVBAR TERMINA-->
+
 
                 <?php
                 // Faz a conexão com o banco de dados
@@ -80,21 +97,39 @@
 
                 // Checa se a busca retornou resultados
                 if (mysqli_num_rows($result) > 0) {
-                    // Cria um loop para exibir os resultados em grupos de 4
-                    while($row = mysqli_fetch_assoc($result)) {
-                        echo '<div class="card swiper-slide">';
-                        echo '<div class="image-content">';
-                        echo '<span class="overlay"></span>';
-                            echo '<div class="card-image">';
-                                echo '<img src="../imagens/img_novelas/'.$row["FOTO_OBRA"].'" alt="'.$row["NOME_OBRA"].'" class="card-img">';
-                            echo '</div>';
+                    $count = 1;  // Inicialize a variável $count
+                
+                    // Abra a div de linha fora do loop
+                    echo '<div class="row row-cols-3 g-3">';  // Inicie a div com as classes desejadas
+                
+                    // Inicie o loop
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $valor_obra = $row['VALOR_OBRA'];
+                        $id_obra = $row['ID_OBRA'];
+                        echo '<div class="col">';  // Coluna para cada card
+                        echo '<div class="card">';  // Card
+                        echo '<a href="../listar_obras_SL.php?id_obra=' . $id_obra . '&valor_obra=' . $valor_obra . '"><img src="../imagens/img_novelas/' . $row['FOTO_OBRA'] . '" alt="' . $row["NOME_OBRA"] . '" class="card-img-top"></a>';
+                        echo '<div class="card-body" style="display: block; text-align: center;">';  
                         echo '</div>';
-                        echo '<div class="card-content">';
-                            echo '<h2 class="name">'.$row["NOME_OBRA"].'</h2>';
-                            echo '<p class="description">'.$row["AU_DI_OBRA"].'</p>';
-                       
+                        echo '</div>';
+                        echo '</div>';
+                
+                        // Verifique se é hora de abrir uma nova linha
+                        if ($count % 3 == 0) {
+                            echo '</div><div class="row row-cols-3 g-3">';  // Feche a linha anterior e inicie uma nova
+                        }
+                
+                        // Incrementa o contador
+                        $count++;
                     }
+                
+                    // Feche a última div de linha após o loop
+                    echo '</div>';
                 }
+                include "../funcoes/functions.php";
+                catRodape();
+                
+                    
 
                 ?>
 
